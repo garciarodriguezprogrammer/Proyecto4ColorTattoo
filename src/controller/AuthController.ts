ca67650c09ff0b2380bf4d5b13c32f2f4d6a5687
Dayanis <dayany1588@yahoo.com>
2024-02-05 18:16:14 +0100

    Se ha creado una funcion para registrar usuarios

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -7,7 +7,7 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.JWT_SECRET_KEY || "123456789";
 //Clase  que  se  encarga  de la autenticacion de los usuarios
 export class AuthController{     
-    //Funcion para registrar usuarios
+    //Funcion  para  registrar  usuarios
     async register(req: Request, res: Response): Promise <void|Response<any>> {   
 
         const {USER_NAME, EMAIL, PASS, ROL} = req.body;

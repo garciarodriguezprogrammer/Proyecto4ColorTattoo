2dee2a2795a051604452911c2b776600cbd7a527
Dayanis <dayany1588@yahoo.com>
2024-02-05 18:21:42 +0100

    Se ha configurado los scripts

--- testConnection.ts
+++
@@ -1,9 +0,0 @@
-// import { error } from 'console';
-// import {createConnection} from 'typeorm';
-
-// createConnection().then(async connection=> {
-//     console.log ("coneccion establecida");
-//     await connection.close();
-// }).catch(error=> {
-//     console.log ("error conexion", error);
-// })

db8aaaf389003e9e4dc667070bfbe711a0d77d9e
Dayanis <dayany1588@yahoo.com>
2024-02-05 18:39:03 +0100

    Se ha modificado server.ts, se ha modificado la ruta para recuperar las citas de los artistas

--- server.ts
+++ server.ts
@@ -42,7 +42,7 @@ app.delete("/deleteAppointment/:id", appointmentsController.deleteAppointment);
 //Recuperar citas de cliente por su id
 app.get("/getAppointmentByClient/:id", appointmentsController.getAppointmentByClient);
 
-//Recuperar  citas de artista
+//Recuperar citas de artista
 app.get("/getAppointmentByArtist/:id", appointmentsController.getAppointmentByArtist);
 
 
